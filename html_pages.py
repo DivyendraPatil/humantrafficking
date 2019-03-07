e3a53007405e74787f653f2003e6447a7dd802b7
Divyendra Patil <divyendrapatil@gmail.com>
2019-03-06 20:18:08 -0500

    Favicon Added

--- html_pages.py
+++ html_pages.py
@@ -4,6 +4,7 @@ welcome = """
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
+  <link rel="shortcut icon" type="image/png" href="/favicon.png"/>
   <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
   <title>Save The Human Project</title>
   <style>
